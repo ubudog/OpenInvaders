c7dc759c2a47c3fa0194342fa0f02cfdd3d1cfb6
ubudog <linuxdude@ubuntu.com>
2012-03-06 23:43:16 -0500

    Hopefully this actually adds the Board class, darn git. :P

---
+++ 0.1/src/net/ubudog/openinvaders/Board.java
@@ -0,0 +1,53 @@
+package net.ubudog.openinvaders;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	static Font font = new Font("SanSerif", Font.BOLD, 25);
+	
+	private Player player; 
+	
+	private Timer time;
+	
+	public Board() {
+		player = new Player(); 
+		addKeyListener(new AL()); 
+		setFocusable(true);
+		
+		time = new Timer(25, this);
+		time.start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+	}
+	
+	public void paint(Graphics g) {
+		
+		/**
+		g.setFont(font);
+		g.setColor(Color.ORANGE);
+		g.drawString("It works!", 300, 200);
+		*/
+		g.drawImage(player.getPlayer(), player.getX(), player.getY(), null);
+	}
+	
+	public class AL extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			player.keyPressed(e);
+		}
+		
+		public void keyReleased(KeyEvent e) {
+			player.keyReleased(e);
+		}
+	}
+}
---
+++ 0.1/src/net/ubudog/openinvaders/Player.java
@@ -0,0 +1,71 @@
+package net.ubudog.openinvaders;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+
+public class Player {
+	
+	Image img;
+	
+	static int x; 
+	static int y; 
+	
+	static String HOME_DIR = System.getProperty("user.home");
+	static String MAIN_DIR = HOME_DIR + "/.openinvaders";
+	
+	public Player() {
+		x = 300; 
+		y = 200;
+		
+		ImageIcon i = new ImageIcon(MAIN_DIR + "/player.png");
+		img = i.getImage();
+	}
+	
+	public void move(int dx, int dy) {
+		x = dx; 
+		y = dy;
+	}
+	
+	public Image getPlayer() {
+		return img; 
+	}
+	
+	public int getX() {
+		return x; 
+	}
+	
+	public int getY() {
+		return y; 
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_W) {
+			y = y - 32;
+		}
+		
+		if (key == KeyEvent.VK_A) { 
+			x = x - 32; 
+		}
+		
+		if (key == KeyEvent.VK_S) {
+			y = y + 32;
+		}
+		
+		if (key == KeyEvent.VK_D) {
+			x = x + 32;
+		}
+		
+		if (key == KeyEvent.VK_ESCAPE) { 
+			System.out.println("Exiting.");
+			System.exit(0);
+		}
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+	}
+	
+}
