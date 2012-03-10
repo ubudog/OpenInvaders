fdd57f46f5772ad9d1758e3fbb5ac574453c200f
ubudog <linuxdude@ubuntu.com>
2012-03-10 17:34:15 -0500

    Adds two 'blocks' of ships to level 2.  Also removes the italics test.

--- 0.1/src/net/ubudog/openinvaders/Board.java
+++ 0.1/src/net/ubudog/openinvaders/Board.java
@@ -27,6 +27,13 @@ public class Board extends JPanel implements ActionListener {
 	private Ship ship6;
 	private Ship ship7;
 	private Ship ship8;
+	private Ship ship9;
+	private Ship ship10;
+	private Ship ship11;
+	private Ship ship12;
+	private Ship ship13;
+	private Ship ship14;
+	private Ship ship15;
 	private Map map;
 	private Timer time;
 	static int level = 1;
@@ -53,6 +60,27 @@ public class Board extends JPanel implements ActionListener {
 	int ship8x;
 	int ship8y;
 	
+	int ship9x;
+	int ship9y;
+	
+	int ship10x;
+	int ship10y;
+	
+	int ship11x;
+	int ship11y;
+	
+	int ship12x;
+	int ship12y;
+	
+	int ship13x;
+	int ship13y;
+	
+	int ship14x;
+	int ship14y;
+	
+	int ship15x;
+	int ship15y;
+	
 	static boolean win = false;
 	static boolean fail = false;
 	
@@ -65,6 +93,13 @@ public class Board extends JPanel implements ActionListener {
 		ship6 = new Ship();
 		ship7 = new Ship();
 		ship8 = new Ship();
+		ship9 = new Ship();
+		ship10 = new Ship();
+		ship11 = new Ship();
+		ship12 = new Ship();
+		ship13 = new Ship();
+		ship14 = new Ship();
+		ship15 = new Ship();
 		map = new Map();
 		player = new Player(); 
 		addKeyListener(new AL()); 
@@ -90,8 +125,32 @@ public class Board extends JPanel implements ActionListener {
 		ship7y = ship.getY();
 		
 		// Level 2 ships
+		
+		// Block of ships
 		ship8x = ship.getX() + 50;
 		ship8y = ship.getY();
+		
+		ship9x = ship.getX() + 100;
+		ship9y = ship.getY();
+		
+		ship10x = ship8x; 
+		ship10y = ship.getY() + 40;
+		
+		ship11x = ship9x;
+		ship11y = ship.getY() + 40;
+		
+		// Block of ships
+		ship12x = ship.getX() + 200;  
+		ship12y = ship.getY();
+		
+		ship13x = ship.getX() + 250;
+		ship13y = ship.getY();
+		
+		ship14x = ship12x;
+		ship14y = ship.getY() + 40;
+		
+		ship15x = ship13x;
+		ship15y = ship.getY() + 40;
 		
 		time = new Timer(25, this);
 		time.start();
@@ -232,15 +291,20 @@ public class Board extends JPanel implements ActionListener {
 				}
 			}
 			g.drawImage(ship8.getShip(), ship8x, ship8y, null);
+			g.drawImage(ship9.getShip(), ship9x, ship9y, null);
+			g.drawImage(ship10.getShip(), ship10x, ship10y, null);
+			g.drawImage(ship11.getShip(), ship11x, ship11y, null);
+			g.drawImage(ship12.getShip(), ship12x, ship12y, null);
+			g.drawImage(ship13.getShip(), ship13x, ship13y, null);
+			g.drawImage(ship14.getShip(), ship14x, ship14y, null);
+			g.drawImage(ship15.getShip(), ship15x, ship15y, null);
+			
 			g.drawImage(player.getPlayer(), player.getX(), player.getY(), null);
 
 			g.setFont(font);
 			g.setColor(Color.ORANGE);
 			g.drawString("You've made it to Level 2!", 50, 250);
 			g.drawString("We're still working on this...", 50, 300);
-			g.setFont(italics);
-			g.setColor(Color.WHITE);
-			g.drawString("Italics test!", 50, 350);
 			
 			if (player.ammo > 0) {
 				g.setFont(font);
