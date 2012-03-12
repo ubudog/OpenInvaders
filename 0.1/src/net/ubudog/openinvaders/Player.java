cd559b5f2768cff9a240b02c8b23546fbb035540
ubudog <linuxdude@ubuntu.com>
2012-03-12 11:42:24 -0500

    Player can only move if he's alive.  :)

--- 0.1/src/net/ubudog/openinvaders/Player.java
+++ 0.1/src/net/ubudog/openinvaders/Player.java
@@ -79,77 +79,80 @@ public class Player {
 	
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		
-		if (getCanMove() == true) {
-			
-			if (key == KeyEvent.VK_W) {
-				if (new Board().getPlayerCanMoveUp() == true) {
-					y = y - 32;
-				} else {
-					System.out.println("Secure the area first!");
+		if (getAlive() == true) {
+			if (getCanMove() == true) {
+				
+				if (key == KeyEvent.VK_W) {
+					if (new Board().getPlayerCanMoveUp() == true) {
+						y = y - 32;
+					} else {
+						System.out.println("Secure the area first!");
+					}
 				}
-			}
-			if (key == KeyEvent.VK_A) {
-				x = x - 32; 
-			}
-			
-			if (key == KeyEvent.VK_S) {
-				if (new Board().getPlayerCanMoveDown() == true) {
+				if (key == KeyEvent.VK_A) {
+					x = x - 32; 
+				}
+				
+				if (key == KeyEvent.VK_S) {
+					if (new Board().getPlayerCanMoveDown() == true) {
+						y = y + 32; 
+					} else {
+						System.out.println("Secure the area first!");
+					}
+				}
+				
+				if (key == KeyEvent.VK_D) {
+					
+					x = x + 32;
+				}
+				
+				if (key == KeyEvent.VK_UP) {
+					y = y - 32; 
+				}
+				
+				if (key == KeyEvent.VK_LEFT) {
+					x = x - 32; 
+				}
+				
+				if (key == KeyEvent.VK_DOWN) {
 					y = y + 32; 
-				} else {
-					System.out.println("Secure the area first!");
 				}
-			}
-			
-			if (key == KeyEvent.VK_D) {
 				
-				x = x + 32;
-			}
-			
-			if (key == KeyEvent.VK_UP) {
-				y = y - 32; 
-			}
-			
-			if (key == KeyEvent.VK_LEFT) {
-				x = x - 32; 
-			}
-			
-			if (key == KeyEvent.VK_DOWN) {
-				y = y + 32; 
-			}
-			
-			if (key == KeyEvent.VK_RIGHT) {
-				x = x + 32;
-			}
-			
-			if (key == KeyEvent.VK_R) {
-				ammo = 20;
-			}
-			
-			if (key == KeyEvent.VK_SPACE) { 
-				//new Ship().alive = false;
-				fireBullet();
-			}
-			
-			if (key == KeyEvent.VK_H) { 
-				// Tell the player the controls
-				System.out.println("Here are the controls:"); 
-				System.out.println("A/Left - Move Left");
-				System.out.println("D/Right - Move Right");
-				System.out.println("Space - Shoot");
-				System.out.println("H - List these controls");
+				if (key == KeyEvent.VK_RIGHT) {
+					x = x + 32;
+				}
 				
+				if (key == KeyEvent.VK_R) {
+					ammo = 20;
+				}
+				
+				if (key == KeyEvent.VK_SPACE) { 
+					//new Ship().alive = false;
+					fireBullet();
+				}
+				
+				if (key == KeyEvent.VK_H) { 
+					// Tell the player the controls
+					System.out.println("Here are the controls:"); 
+					System.out.println("A/Left - Move Left");
+					System.out.println("D/Right - Move Right");
+					System.out.println("Space - Shoot");
+					System.out.println("H - List these controls");
+					
+				}
+				
+			} else {
+				System.out.println("I can't move.  I'm out of bounds!");
 			}
 			
-		} else {
-			System.out.println("I can't move.  I'm out of bounds!");
+		} else if (getAlive() == false) {
+			
 		}
 		
 		if (key == KeyEvent.VK_ESCAPE) { 
 			System.out.println("Exiting.");
 			System.exit(0);
 		}
-		
 	}
 	
 	public void keyReleased(KeyEvent e) {
