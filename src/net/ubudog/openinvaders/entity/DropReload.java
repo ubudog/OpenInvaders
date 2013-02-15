31c3985f1b894ef870bae5cddde91b4e61413b6d
ubudog <ubudog@ubudog.net>
2013-02-14 21:41:00 -0500

    Adds the DropReload class.

---
+++ src/net/ubudog/openinvaders/entity/DropReload.java
@@ -0,0 +1,57 @@
+package net.ubudog.openinvaders.entity;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+/*This file is part of OpenInvaders.
+
+OpenInvaders is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenInvaders is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenInvaders.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+public class DropReload {
+	int x, y;
+	
+	Image img; 
+	
+	public boolean visible; 
+	
+	public DropReload(int x, int y) {
+		ImageIcon dropReload = new ImageIcon("res/drawable/dropreload.gif"); 
+		img = dropReload.getImage(); 
+		
+		visible = true; 
+	}
+	
+	public Rectangle getBounds() { 
+		return new Rectangle(x, y, 32, 32); 
+	}
+	
+	public boolean getVisible() { 
+		return visible;
+	}
+	
+	public Image getDrop() { 
+		return img; 
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() { 
+		return y; 
+	}
+}
